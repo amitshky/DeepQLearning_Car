2c2b27365ff66b95a0ec55a5d494da0b11f8c026
LorisDonovan <a7shky@gmail.com>
2020-10-29 23:40:14 +0545

    update

--- Source/DQN_Car/CarGI.cpp
+++ Source/DQN_Car/CarGI.cpp
@@ -26,33 +26,34 @@ const std::string UCarGI::FilePath = UCarGI::RootPath + "Policy.pt";
 
 torch::Device UCarGI::Device = torch::kCPU; // using gpu is slower because UE4 also uses gpu (maybe)
 
+// dont set these yet // crashes editor // smart pointers (policyNet and targetNet) not getting deallocated or had already been deallocated before destructor
 //std::unique_ptr<ReplayMemory>     UCarGI::Mem = std::make_unique<ReplayMemory>(Capacity, NumStates, Device);
 //std::unique_ptr<QAgent>           UCarGI::Agent = std::make_unique<QAgent>(NumActions, g_Start, g_End, g_Decay, Device);
-std::unique_ptr<NetworkInterface> UCarGI::Net = std::make_unique<NetworkInterface>(NumStates, NumActions, g_Gamma, g_LearningRate, Device);
+//std::unique_ptr<NetworkInterface> UCarGI::Net = std::make_unique<NetworkInterface>(NumStates, NumActions, g_Gamma, g_LearningRate, Device);
 
 
 UCarGI::UCarGI()
 {
 	UE_LOG(LogTemp, Warning, TEXT("CarGI Constructor"));
 
-	Net->Init();
+	//Net->Init();
 
-	// resume training
-	if (g_Resume)
-	{
-		Net->LoadPolicyNet(FilePath);
-		Net->LoadTargetNet(FilePath);
-		Net->LoadOptimizer(RootPath + "Optimizer.pt");
-		//Mem->LoadReplayMem(RootPath);
-		// load hyperparameters
-		g_Resume = false; // so that replay mem won't be loaded again
-	}
-	else
-	{
-		// copying parameters of the policyNet to targetNet
-		Net->CloneModel(FilePath);
-		// LoadStateDict(m_TargetNet, m_PolicyNet); // doesnt work yet
-	}
+	//// resume training
+	//if (g_Resume)
+	//{
+	//	Net->LoadPolicyNet(FilePath);
+	//	Net->LoadTargetNet(FilePath);
+	//	Net->LoadOptimizer(RootPath + "Optimizer.pt");
+	//	//Mem->LoadReplayMem(RootPath);
+	//	// load hyperparameters
+	//	g_Resume = false; // so that replay mem won't be loaded again
+	//}
+	//else
+	//{
+	//	// copying parameters of the policyNet to targetNet
+	//	Net->CloneModel(FilePath);
+	//	// LoadStateDict(m_TargetNet, m_PolicyNet); // doesnt work yet
+	//}
 	//UE_LOG(LogTemp, Warning, TEXT("Replay Mem size = %lld"), Mem->GetMemorySize());
 }
 
@@ -61,8 +62,8 @@ UCarGI::~UCarGI()
 	UE_LOG(LogTemp, Warning, TEXT("CarGI Destructor"));
 	//UE_LOG(LogTemp, Warning, TEXT("Replay Mem size = %lld"), Mem->GetMemorySize());
 
-	Net->SavePolicyNet(FilePath);
-	Net->SaveOptimizer(RootPath + "Optimizer.pt");
+	//Net->SavePolicyNet(FilePath);
+	//Net->SaveOptimizer(RootPath + "Optimizer.pt");
 	//Mem->SaveReplayMem(Mem->GetMemorySize(), RootPath);
 	// serialize hyperparameters
 }
