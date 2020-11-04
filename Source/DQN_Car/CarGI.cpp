7b2dc8f701ece2bfa7075c8ef7a883cbcc05e2cb
LorisDonovan <a7shky@gmail.com>
2020-11-04 19:04:36 +0545

    saved files backup

--- Source/DQN_Car/CarGI.cpp
+++ Source/DQN_Car/CarGI.cpp
@@ -32,9 +32,9 @@ const std::string UCarGI::FilePath = UCarGI::RootPath + "Policy.pt";
 torch::Device UCarGI::Device = torch::kCPU; // using gpu is slower because UE4 also uses gpu (maybe)
 
 // dont set these yet // crashes editor // smart pointers (policyNet and targetNet) not getting deallocated or had already been deallocated before destructor
-std::unique_ptr<ReplayMemory>     UCarGI::Mem = std::make_unique<ReplayMemory>(Capacity, NumStates, Device);
+std::unique_ptr<ReplayMemory>     UCarGI::Mem   = std::make_unique<ReplayMemory>(Capacity, NumStates, Device);
 std::unique_ptr<QAgent>           UCarGI::Agent = std::make_unique<QAgent>(NumActions, g_Start, g_End, g_Decay, Device);
-std::unique_ptr<NetworkInterface> UCarGI::Net = std::make_unique<NetworkInterface>(NumStates, NumActions, g_Gamma, g_LearningRate, Device);
+std::unique_ptr<NetworkInterface> UCarGI::Net   = std::make_unique<NetworkInterface>(NumStates, NumActions, g_Gamma, g_LearningRate, Device);
 
 
 UCarGI::UCarGI()
@@ -60,15 +60,15 @@ UCarGI::UCarGI()
 		Net->CloneModel(FilePath);
 		// LoadStateDict(m_TargetNet, m_PolicyNet); // doesnt work yet
 	}
-	UE_LOG(LogTemp, Warning, TEXT("Replay Mem size = %lld"), Mem->GetMemorySize());
-	UE_LOG(LogTemp, Warning, TEXT("VecRewards size = %lld"), VecRewards.size());
+	UE_LOG(LogTemp, Warning, TEXT("Replay Mem size = %llu"), Mem->GetMemorySize());
+	UE_LOG(LogTemp, Warning, TEXT("VecRewards size = %llu"), VecRewards.size());
 }
 
 UCarGI::~UCarGI()
 {
 	UE_LOG(LogTemp, Warning, TEXT("CarGI Destructor"));
-	UE_LOG(LogTemp, Warning, TEXT("Replay Mem size = %lld"), Mem->GetMemorySize());
-	UE_LOG(LogTemp, Warning, TEXT("VecRewards size = %lld"), VecRewards.size());
+	UE_LOG(LogTemp, Warning, TEXT("Replay Mem size = %llu"), Mem->GetMemorySize());
+	UE_LOG(LogTemp, Warning, TEXT("VecRewards size = %llu"), VecRewards.size());
 
 	Net->SavePolicyNet(FilePath);
 	Net->SaveOptimizer(RootPath + "Optimizer.pt");
