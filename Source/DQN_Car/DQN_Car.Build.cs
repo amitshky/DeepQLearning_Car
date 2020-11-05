b9b455da51c108696ea50772295df74f9e717440
Nikesh <eminem.nikesh@gmail.com>
2020-11-05 16:38:07 +0545

    nothing much

--- Source/DQN_Car/DQN_Car.Build.cs
+++ Source/DQN_Car/DQN_Car.Build.cs
@@ -20,7 +20,7 @@ public class DQN_Car : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 
-		string libtorch_path = "C:/_MyFiles/VisualStudio/@Libraries/PytorchForCpp/release";
+		string libtorch_path = "C:/libtorch";
 		string libtorch_include = "libtorch/include";
 		string libtorch_torch_include = "libtorch/include/torch/csrc/api/include";
 		string libtorch_lib = "libtorch/lib";
